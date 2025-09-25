1f6c290dd911d3baa591c78c3c36a2faa42b0e98
Dave-III <seakinsconnor@gmail.com>
2025-09-25 14:48:17 +1200

    setup FEN encoder

--- Chess Framework/Chessboard.py
+++ Chess Framework/Chessboard.py
@@ -17,23 +17,23 @@ class ChessBoard:
         board = [[' ' for _ in range(8)] for _ in range(8)]
         # Place pawns
         for i in range(8):
-            board[1][i] = 'P'  # White pawns
-            board[6][i] = 'p'  # Black pawns
+            board[1][i] = 'p'  # Black pawns
+            board[6][i] = 'P'  # White pawns
         # Place rooks
-        board[0][0] = board[0][7] = 'R'
-        board[7][0] = board[7][7] = 'r'
+        board[0][0] = board[0][7] = 'r'
+        board[7][0] = board[7][7] = 'R'
         # Place knights
-        board[0][1] = board[0][6] = 'N'
-        board[7][1] = board[7][6] = 'n'
+        board[0][1] = board[0][6] = 'n'
+        board[7][1] = board[7][6] = 'N'
         # Place bishops
-        board[0][2] = board[0][5] = 'B'
-        board[7][2] = board[7][5] = 'b'
+        board[0][2] = board[0][5] = 'b'
+        board[7][2] = board[7][5] = 'B'
         # Place queens
-        board[0][3] = 'Q'
-        board[7][3] = 'q'
+        board[0][3] = 'q'
+        board[7][3] = 'Q'
         # Place kings
-        board[0][4] = 'K'
-        board[7][4] = 'k'
+        board[0][4] = 'k'
+        board[7][4] = 'K'
         return board
 
     def display(self):
@@ -66,6 +66,25 @@ class ChessBoard:
         piece = self.board[row][col]
         return self.PIECE_MAP[piece]
 
+    def board_to_FEN(self):
+        # Convert the board to Forsyth-Edwards Notation (FEN)
+        fen = ""
+        for row in self.board:
+            empty_count = 0
+            for piece in row:
+                if piece == ' ':
+                    empty_count += 1
+                else:
+                    if empty_count > 0:
+                        fen += str(empty_count)
+                        empty_count = 0
+                    fen += piece
+            if empty_count > 0:
+                fen += str(empty_count)
+            fen += '/'
+        return fen[:-1]  # Remove the trailing '/'
+    
+
 # Example usage
 if __name__ == "__main__":
     chess_board = ChessBoard()
@@ -73,6 +92,7 @@ if __name__ == "__main__":
     print(chess_board.board_state()) # Display board state as string of indices
 
     # Get user input for a position
+    print(chess_board.board_to_FEN())
     x = input("Enter row: ")
     y = input("Enter column: ")
     user_input = (int(x), int(y)) # Convert input to tuple of integers
