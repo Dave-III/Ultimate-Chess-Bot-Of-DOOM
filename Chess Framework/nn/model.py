2c194b79d774955489b1b5ad2e44c5b3dd61b811
Dave-III <seakinsconnor@gmail.com>
2025-10-03 00:08:52 +1300

    quick commit

--- Chess Framework/nn/model.py
+++ Chess Framework/nn/model.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import constants as const
-from board.Chessboard import ChessBoard
 
 class ChessModel(nn.Module):
     def __init__(self):
@@ -9,7 +8,8 @@ class ChessModel(nn.Module):
         # Define the neural network layers
 
         #input consists of 256 values (64 squares * 4 bits per square)
-        #layer1 will have 512 connections to the next layer.
+        #layer1 will have 512 connections to the next layer. 512 nodes was chosen
+        #to give the model more capacity to learn complex patterns.
         self.layer1 = nn.Linear(256, 512) 
 
         #layer2 will take 512 values from layer1.
@@ -19,3 +19,14 @@ class ChessModel(nn.Module):
         #layer3 will take 256 values from layer2.
         #layer3 will have 1 connection to the output layer, which will be the value fed to the search algorithm.
         self.layer3 = nn.Linear(256, 1)
+
+    #values is the input to the model
+    #from the chessboard function board_state().
+    def forward(self, values):
+
+        #Apply ReLU activation function after each layer except the last one
+        #ReLU was chosen for its efficiency and effectiveness in deep learning
+        #models because it helps to mitigate the vanishing gradient problem.
+        values = F.relu(self.layer1(values))
+        values = F.relu(self.layer2(values))
+        return self.layer3(values)
